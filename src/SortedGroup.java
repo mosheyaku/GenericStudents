4b054cf0140fbddeecdfb5f9ea0861eacfde0ed4
Moshe <mosheyaku@gmail.com>
2023-05-19 15:00:17 +0300

    Adding constructor

--- src/SortedGroup.java
+++ src/SortedGroup.java
@@ -1,3 +1,10 @@
-public class SortedGroup {
+import java.util.ArrayList;
 
+public class SortedGroup <T extends Comparable<T>>{
+
+    private ArrayList<T> collection;
+
+    public SortedGroup(){
+        collection= new ArrayList<>();
+    }
 }
