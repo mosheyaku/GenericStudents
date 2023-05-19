13b65c66ea0727d6b73e15cb6518501db22e3f3f
Moshe <mosheyaku@gmail.com>
2023-05-19 16:34:21 +0300

    Adding reduce method

--- src/OperationsOnCollections.java
+++ src/OperationsOnCollections.java
@@ -1,3 +1,18 @@
+import java.util.Iterator;
+
 public class OperationsOnCollections {
 
+    public static <T extends Comparable<T>> SortedGroup<T> reduce(SortedGroup<T> sGroup, T x) {
+        SortedGroup<T> result = new SortedGroup<>();
+
+        Iterator<T> iterator = sGroup.iterator();
+        while (iterator.hasNext()) {
+            T collection = iterator.next();
+            if (collection.compareTo(x) > 0) {
+                result.add(collection);
+            }
+        }
+
+        return result;
+    }
 }
