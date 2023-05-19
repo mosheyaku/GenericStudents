37af841b0cf659b20f5a87fe7026577b319aa4b2
Moshe <mosheyaku@gmail.com>
2023-05-19 15:57:14 +0300

    Adding remove and iterator methods

--- src/SortedGroup.java
+++ src/SortedGroup.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class SortedGroup <T extends Comparable<T>>{
+public class SortedGroup <T extends Comparable<T>> implements Iterable<T>{
 
     private ArrayList<T> collection;
 
@@ -16,7 +17,20 @@ public class SortedGroup <T extends Comparable<T>>{
         collection.add(index, element);
     }
 
+    public int remove(T element) {
+        int count = 0;
+        Iterator<T> iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().equals(element)) {
+                iterator.remove();
+                count++;
+            }
+        }
+        return count;
+    }
 
-
-
+    @Override
+    public Iterator<T> iterator() {
+        return collection.iterator();
+    }
 }
