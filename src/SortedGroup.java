f4254851ec7bdd0e22b069e6cbf5d1dbb334fd65
Moshe <mosheyaku@gmail.com>
2023-05-19 15:48:37 +0300

    Adding method: add

--- src/SortedGroup.java
+++ src/SortedGroup.java
@@ -7,4 +7,16 @@ public class SortedGroup <T extends Comparable<T>>{
     public SortedGroup(){
         collection= new ArrayList<>();
     }
+
+    public void add(T element) {
+        int index = 0;
+        while (index < collection.size() && element.compareTo(collection.get(index)) > 0) {
+            index++;
+        }
+        collection.add(index, element);
+    }
+
+
+
+
 }
