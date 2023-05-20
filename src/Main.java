f39e877084f552f60edee6a39e24a58eeb5a137b
Moshe <mosheyaku@gmail.com>
2023-05-21 02:03:30 +0300

    Rewriting the main with the use of helper methods

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,54 @@
 public class Main {
 
+    private static SortedGroup<Student> group;
+
+    private static void addStudentsCollection() {
+        addNewStudent("Chris pratt", 111, 80);
+        addNewStudent("Dave Bautista", 222, 59);
+        addNewStudent("Zoe Saldana", 333, 70);
+        addNewStudent("Karen Gillan", 444, 90);
+        addNewStudent("Vin Diesel", 555, 55);
+        addNewStudent("Bradley Cooper", 666, 59);
+    }
+
+    private static void addNewStudent(String name, int id, int grade) {
+        Student student = new Student(name, id, grade);
+        group.add(student);
+    }
+
+
+    private static void printCollection(SortedGroup<Student> group, String message) {
+        System.out.println(message);
+        printStudents(group);
+    }
+
     private static void printStudents(SortedGroup<Student> group) {
         for (Student student : group) {
             System.out.println(student.toString());
         }
     }
 
-    private static void addNewStudent(SortedGroup<Student> group, String name, int id, int grade) {
-        Student student = new Student(name, id, grade);
-        group.add(student);
+    private static void removeStudentsWithSameGrade(String name, int id, int grade){
+        System.out.printf("\nRemove all students with a %d grade:",grade);
+        Student removeSameGrade = new Student(name, id, grade);
+        int removedCount = group.remove(removeSameGrade);
+        System.out.println("\nRemoved " + removedCount + " students.");
     }
 
     public static void main(String[] args) {
-        SortedGroup<Student> group = new SortedGroup<>();
+        group = new SortedGroup<>();
+        addStudentsCollection();
 
-        addNewStudent(group, "Chris pratt", 111, 80);
-        addNewStudent(group, "Dave Bautista", 222, 59);
-        addNewStudent(group, "Zoe Saldana", 333, 70);
-        addNewStudent(group, "Karen Gillan", 444, 90);
-        addNewStudent(group, "Vin Diesel", 555, 55);
-        addNewStudent(group, "Bradley Cooper", 666, 59);
+        printCollection(group,"Original Collection:");
 
-        /*
-        Student student1 = new Student("Chris pratt", 111, 80);
-        Student student2 = new Student("Dave Bautista", 222, 60);
-        Student student3 = new Student("Zoe Saldana", 333, 70);
-        Student student4 = new Student("Karen Gillan", 444, 90);
-        Student student5 = new Student("Vin Diesel", 555, 55);
-        Student student6 = new Student("Bradley Cooper", 666, 60);
+        removeStudentsWithSameGrade("William Poulter", 777, 59);
 
-
-         group.add(student1);
-        group.add(student2);
-        group.add(student3);
-        group.add(student4);
-        group.add(student5);
-        group.add(student6);
-        */
-
-        System.out.println("Original Collection:");
-        printStudents(group);
-
-        System.out.println("Remove all students with a 60 grade:");
-        Student studentToRemove = new Student("William Poulter", 777, 59);
-        int removedCount = group.remove(studentToRemove);
-        System.out.println("\nRemoved " + removedCount + " students.");
-
-
-        System.out.println("\nUpdated Collection:");
-        printStudents(group);
-
+        printCollection(group,"\nUpdated Collection:");
 
         Student thresholdStudent = new Student("Sean Gunn", 888, 60);
         SortedGroup<Student> reducedGroup = OperationsOnCollections.reduce(group, thresholdStudent);
 
-        System.out.println("\nReduced Collection (showing students that has grade greater than 60):");
-        printStudents(reducedGroup);
+        printCollection(reducedGroup,"\nReduced Collection (showing students that has grade greater than 60):");
+
     }
 }
