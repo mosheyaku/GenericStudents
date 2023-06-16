5f1f00f81512e6a6248008be850570c28b81a0bf
Moshe <mosheyaku@gmail.com>
2023-06-16 15:41:04 +0300

    Adding documentation

--- src/SortedGroup.java
+++ src/SortedGroup.java
@@ -1,14 +1,20 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
+/*A generic class named SortedGroup used to define a collection of sorted members
+in ascending order of some type T that implements the Comparable<T> interface.*/
 public class SortedGroup<T extends Comparable<T>> implements Iterable<T> {
 
     private ArrayList<T> collection;
 
+    /*A constructor that initializes the collection*/
     public SortedGroup() {
         collection = new ArrayList<>();
     }
 
+    /*The add method accepts an element and adds it to the collection in
+    its appropriate place so that the sorting is preserved.
+    It is possible that the collection will include duplicate members.*/
     public void add(T element) {
         int index = 0;
         while (index < collection.size() && element.compareTo(collection.get(index)) > 0) {
@@ -17,6 +23,9 @@ public class SortedGroup<T extends Comparable<T>> implements Iterable<T> {
         collection.add(index, element);
     }
 
+    /*The remove method accepts a member and removes from the collection all
+    the members that are equal (according to equals) to the given member.
+    The method will return a number indicating how many members were removed.*/
     public int remove(T element) {
         int count = 0;
         Iterator<T> iterator = collection.iterator();
@@ -29,6 +38,7 @@ public class SortedGroup<T extends Comparable<T>> implements Iterable<T> {
         return count;
     }
 
+    /*The iterator method returns an iterator of the generic type.*/
     @Override
     public Iterator<T> iterator() {
         return collection.iterator();
